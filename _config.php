822b0588bf91286246b57b60821c4bb02739649b
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-04-30 21:41:03 +1200

    FIX: post SS6 Upgrade fixes

--- _config.php
+++ _config.php
@@ -4,12 +4,10 @@ use Sunnysideup\Sitemapable\Interfaces\SitemapableInterface;
 use SilverStripe\Core\ClassInfo;
 use Wilr\GoogleSitemaps\GoogleSitemap;
 
-if(class_exists('\\Wilr\\GoogleSitemaps\\GoogleSitemap')) {
-    if (isset($_SERVER['REQUEST_URI'])) {
-        if (0 === strpos(ltrim((string) $_SERVER['REQUEST_URI'], '/'), 'sitemap')) {
-            foreach(ClassInfo::implementorsOf(SitemapableInterface::class) as $className) {
-                GoogleSitemap::register_dataobject($className, 'daily');
-            }
+if(class_exists(GoogleSitemap::class) && isset($_SERVER['REQUEST_URI'])) {
+    if (str_starts_with(ltrim((string) $_SERVER['REQUEST_URI'], '/'), 'sitemap')) {
+        foreach(ClassInfo::implementorsOf(SitemapableInterface::class) as $className) {
+            GoogleSitemap::register_dataobject($className, 'daily');
         }
     }
 }
--- src/Interfaces/SitemapableInterface.php
+++ src/Interfaces/SitemapableInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sunnysideup\Sitemapable\Interfaces;
 
 interface SitemapableInterface
