be01cef588e2f5f0acba0baf050bf6f17aea6b99
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-05-08 11:32:25 +1200

    FIX: ss6 cleanup

--- _config.php
+++ _config.php
@@ -4,10 +4,8 @@ use Sunnysideup\Sitemapable\Interfaces\SitemapableInterface;
 use SilverStripe\Core\ClassInfo;
 use Wilr\GoogleSitemaps\GoogleSitemap;
 
-if(class_exists(GoogleSitemap::class) && isset($_SERVER['REQUEST_URI'])) {
-    if (str_starts_with(ltrim((string) $_SERVER['REQUEST_URI'], '/'), 'sitemap')) {
-        foreach(ClassInfo::implementorsOf(SitemapableInterface::class) as $className) {
-            GoogleSitemap::register_dataobject($className, 'daily');
-        }
+if(class_exists(GoogleSitemap::class) && isset($_SERVER['REQUEST_URI']) && str_starts_with(ltrim((string) $_SERVER['REQUEST_URI'], '/'), 'sitemap')) {
+    foreach(ClassInfo::implementorsOf(SitemapableInterface::class) as $className) {
+        GoogleSitemap::register_dataobject($className, 'daily');
     }
 }
